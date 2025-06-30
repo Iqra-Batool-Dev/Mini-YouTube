d58c2f33581d18d2afe5a3b88f8816b852fe4c14
Hamid Raza <hamidramay2002@gmail.com>
2025-06-30 16:03:43 +0500

    refactor: restructure application layout and remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-import { Outlet } from "react-router-dom";
-import { Header, Sidebar, Sidebar2 } from "./components/index";
+import MainLayout from "./components/Layout/MainLayout";
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   return (
-    <div className=" w-full flex flex-col min-h-screen flex-wrap ">
-      <Header onMenuToggle={toggleSidebar} />
-      <Sidebar2 isOpen={isSidebarOpen} onMenuToggle={toggleSidebar} />
-      <div className=" w-full flex flex-row  flex-1">
-        <Sidebar />
-        <main className=" w-[100%] xl:w-[85%] px-10 py-2 border-amber-200 border-2 ">
-          Home part:
-          <Outlet />
-        </main>
-      </div>
-    </div>
+    <>
+      <MainLayout />
+    </>
   );
 }
 
---
+++ src/pages/Bookmark.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Bookmark() {
+  return (
+    <div>
+      Bookmark
+    </div>
+  )
+}
+
+export default Bookmark
+Bookmark
